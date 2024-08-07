d84a950cc34df0874e8abb49cc76f038341474b9
LevonKad <levonkadehjian20@gmail.com>
2024-08-06 21:50:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+/ Middleware to parse the body of the request
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve the HTML form
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Handle form submission
 app.post('/submit', (req, res) => {
-    console.log('Got body:', req.body);
-    res.send(`Received your request! Name: ${req.body.username}`);
+    const name = req.body.name;
+    const phone = req.body.phone;
+
+    // Regular expression to validate phone number format ddd-ddd-dddd
+    const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+
+    if (phoneRegex.test(phone)) {
+        res.send(`Hello, ${name}. The phone number ${phone} is valid.`);
+    } else {
+        res.send(`Hello, ${name}. The phone number ${phone} is invalid. Please enter in the format ddd-ddd-dddd.`);
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
